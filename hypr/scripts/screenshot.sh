a7a8c32a64e30c26d39a0035c8ba01d1886b2dac
0xdya <0xdya@proton.me>
2025-11-25 14:37:53 +0100

    Delete hypr directory

--- hypr/scripts/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create screenshot directory
-mkdir -p ~/Pictures/Screenshots
-
-# Capture screenshot
-FILE=~/Pictures/Screenshots/Screenshot-$(date +%F_%T).png
-
-grimblast --notify copysave area $FILE
